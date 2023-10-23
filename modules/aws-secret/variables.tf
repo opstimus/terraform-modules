d7b9f54d2a5713c5422485849c6349155dab093c
aabidh-op <142312703+aabidh-op@users.noreply.github.com>
2023-10-23 16:31:19 +0530

    move-module-code (#1)
    LGTM

---
+++ modules/aws-secret/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  type        = string
+  description = "Project name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
+}
+
+variable "name" {
+  type        = string
+  description = "Secret name | i.e mail-password"
+}
