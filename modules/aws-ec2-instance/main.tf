91c6b54b9cd9bf40d3903ab891cc1c46c159cd5b
Checkboxer <131902302+checkboxer-op@users.noreply.github.com>
2023-12-20 16:07:16 +0530

    DEVOPS-160 - ec2 instance (#1)
    Co-authored-by: Checkboxer <checkboxer@friedlemons.org>

---
+++ modules/aws-ec2-instance/main.tf
@@ -0,0 +1,70 @@
+resource "aws_security_group" "main" {
+  count       = length(var.ingress_rules) > 0 ? 1 : 0
+  name        = "${var.project}-${var.environment}-${var.name}"
+  description = "${var.project}-${var.environment}-${var.name}"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  # All traffic enabled for outbound. Restrict if required
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.project}-${var.environment}-${var.name}"
+  }
+}
+
+resource "aws_instance" "main" {
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = length(var.ingress_rules) > 0 ? [aws_security_group.main[0].id] : var.security_group_ids
+  source_dest_check       = var.source_dest_check
+  subnet_id               = var.subnet_id
+  key_name                = var.key_name != null ? var.key_name : null
+  disable_api_termination = var.termination_protection ? true : false
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = var.root_volume_size
+    volume_type           = "gp3"
+    encrypted             = true
+  }
+
+  user_data_replace_on_change = true
+  user_data                   = var.user_data
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  tags = {
+    "Name" = "${var.project}-${var.environment}-${var.name}"
+  }
+}
+
+resource "aws_eip" "main" {
+  tags = {
+    "Name" = "${var.project}-${var.environment}-${var.name}"
+  }
+}
+
+resource "aws_eip_association" "main" {
+  count         = var.enable_eip ? 1 : 0
+  instance_id   = aws_instance.main.id
+  allocation_id = aws_eip.main.id
+}
---
+++ modules/aws-ec2-instance/variables.tf
@@ -0,0 +1,84 @@
+variable "project" {
+  type        = string
+  description = "Project name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
+}
+
+variable "name" {
+  type        = string
+  description = "Instance name | i.e api"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "root_volume_size" {
+  type        = number
+  description = "Volume size in GB"
+  default     = 10
+}
+
+variable "ami" {
+  type    = string
+  default = "AMI ID"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "VPC cidr"
+}
+
+variable "enable_eip" {
+  type        = bool
+  description = "Enable and attach Elastic IP"
+  default     = false
+}
+
+variable "user_data" {
+  type        = string
+  description = "Userdata"
+}
+
+variable "source_dest_check" {
+  type    = bool
+  default = true
+}
+
+variable "security_group_ids" {
+  type        = list(any)
+  description = "Security group ID"
+  default     = []
+}
+
+variable "vpc_id" {
+  description = "VPC ID needed to create security group"
+  type        = string
+  default     = null
+}
+
+variable "ingress_rules" {
+  description = "List of security group ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
+
+variable "key_name" {
+  type    = string
+  default = null
+}
+
+variable "termination_protection" {
+  type    = bool
+  default = false
+}
