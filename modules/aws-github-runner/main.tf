ebf87836529c6ee38d024cad86e44e1a72d41747
Checkboxer <checkboxer@friedlemons.org>
2026-01-04 15:04:49 +0530

    Updates AMI filter for Ubuntu 24.04
    Updates the AMI filter to correctly identify the Ubuntu 24.04 image and adds an architecture filter.
    
    Ensures that the correct Ubuntu 24.04 AMI is selected for the runner instance, improving reliability and consistency.
    
    Relates to DEVOPS-379-module

--- modules/aws-github-runner/main.tf
+++ modules/aws-github-runner/main.tf
@@ -57,12 +57,17 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-24.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
