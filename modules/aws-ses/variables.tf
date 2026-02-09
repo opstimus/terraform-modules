fecbff63fc56599b93f195c9c97e3e4fb76f5b50
suhail-op <suhailfazil@opstimus.com>
2026-02-09 11:42:35 +0530

    Update variables.tf

--- modules/aws-ses/variables.tf
+++ modules/aws-ses/variables.tf
@@ -6,9 +6,11 @@ variable "ses_domain" {
 variable "ses_email" {
   type        = string
   description = "Email address for SES email identity"
+  default     = null
 }
 
 variable "identity_type" {
   type        = string
   description = "Type of SES identity to create (domain or email)"
+  default     = null
 }
