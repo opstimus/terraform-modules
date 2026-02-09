7f5ffbcfb7211b40bd3ad69e21d747269d4e84bf
suhail-op <suhailfazil@opstimus.com>
2026-02-09 11:35:52 +0530

    DEVOPS-409 Create terraform-aws-ses

---
+++ modules/aws-ses/variables.tf
@@ -0,0 +1,14 @@
+variable "ses_domain" {
+  type        = string
+  description = "Domain name (domain.com or subdomain.domain.com)"
+}
+
+variable "ses_email" {
+  type        = string
+  description = "Email address for SES email identity"
+}
+
+variable "identity_type" {
+  type        = string
+  description = "Type of SES identity to create (domain or email)"
+}
