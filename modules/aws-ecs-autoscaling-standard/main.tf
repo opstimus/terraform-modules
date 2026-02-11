568ea84243c9cf58674c0b1aa9868784749f0d83
mfmsuhail <82133621+mfmsuhail@users.noreply.github.com>
2026-02-11 10:12:18 +0530

    Devops 245 auto scale using custom metric (#3)
    * DEVOPS-245: Auto scale using custom metrics
    
    * Change condition pattern
    
    * Rename variables
    
    Rename variables
    1. custom_metric_name
    2. custom_namespace
    3. custom_statistic
    
    Add variables
    1. custom_dimension_name
    2. custom_dimension_value
    
    * Rename variables
    
    * Rename variables
    
    * Update ci.yml
    
    ---------
    
    Co-authored-by: suhail-op <suhailfazil@opstimus.com>

--- modules/aws-ecs-autoscaling-standard/main.tf
+++ modules/aws-ecs-autoscaling-standard/main.tf
@@ -7,6 +7,7 @@ resource "aws_appautoscaling_target" "main" {
 }
 
 resource "aws_appautoscaling_policy" "cpu" {
+  count              = var.cpu_target_value != null ? 1 : 0
   name               = "${var.service_name}-cpu"
   policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.main.resource_id
@@ -25,6 +26,7 @@ resource "aws_appautoscaling_policy" "cpu" {
 }
 
 resource "aws_appautoscaling_policy" "memory" {
+  count              = var.memory_target_value != null ? 1 : 0
   name               = "${var.service_name}-memory"
   policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.main.resource_id
@@ -39,5 +41,31 @@ resource "aws_appautoscaling_policy" "memory" {
     target_value       = var.memory_target_value
     scale_in_cooldown  = var.scale_in_cooldown
     scale_out_cooldown = var.scale_out_cooldown
+  }
+}
+
+resource "aws_appautoscaling_policy" "custom_metric_scale" {
+  count              = var.custom_metric_target_value != null ? 1 : 0
+  name               = "${var.service_name}-custom-metric-scale"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.main.resource_id
+  scalable_dimension = aws_appautoscaling_target.main.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.main.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    customized_metric_specification {
+      metric_name = var.custom_metric_name
+      namespace   = var.custom_metric_namespace
+      statistic   = var.custom_metric_statistic
+
+      dimensions {
+        name  = var.custom_metric_dimension_name
+        value = var.custom_metric_dimension_value
+      }
+    }
+
+    target_value       = var.custom_metric_target_value
+    scale_in_cooldown  = var.custom_metric_scale_in_cooldown
+    scale_out_cooldown = var.custom_metric_scale_out_cooldown
   }
 }
--- modules/aws-ecs-autoscaling-standard/variables.tf
+++ modules/aws-ecs-autoscaling-standard/variables.tf
@@ -16,12 +16,17 @@ variable "max_capacity" {
 
 variable "cpu_target_value" {
   type    = number
-  default = 60
+  default = null
 }
 
 variable "memory_target_value" {
   type    = number
-  default = 60
+  default = null
+}
+
+variable "custom_metric_target_value" {
+  type    = number
+  default = null
 }
 
 variable "scale_in_cooldown" {
@@ -29,7 +34,35 @@ variable "scale_in_cooldown" {
   default = 300
 }
 
+variable "custom_metric_scale_in_cooldown" {
+  type = number
+}
+
 variable "scale_out_cooldown" {
   type    = number
   default = 60
+}
+
+variable "custom_metric_scale_out_cooldown" {
+  type = number
+}
+
+variable "custom_metric_name" {
+  type = string
+}
+
+variable "custom_metric_namespace" {
+  type = string
+}
+
+variable "custom_metric_statistic" {
+  type = string
+}
+
+variable "custom_metric_dimension_name" {
+  type = string
+}
+
+variable "custom_metric_dimension_value" {
+  type = string
 }
