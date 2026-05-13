ee7ade06a06e2dd60160e4430510ffda60f72095
suhail-op <suhailfazil@opstimus.com>
2026-05-13 08:48:40 +0530

    Output management_endpoint

--- modules/oci-kms-vault/outputs.tf
+++ modules/oci-kms-vault/outputs.tf
@@ -3,3 +3,7 @@ output "vault_id" {
   value       = oci_kms_vault.main.id
 }
 
+output "management_endpoint" {
+  description = "The management endpoint of the KMS vault."
+  value       = oci_kms_vault.main.management_endpoint
+}
