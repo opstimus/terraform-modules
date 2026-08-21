44c97e8a0ce8aceb307126ecd3c4a941776fbd1f
suhail-op <suhailfazil@opstimus.com>
2026-08-21 15:17:35 +0530

    Restrict S3 bucket encryption settings

--- modules/aws-s3-bucket/main.tf
+++ modules/aws-s3-bucket/main.tf
@@ -23,6 +23,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
   bucket = aws_s3_bucket.main.id
 
   rule {
+    blocked_encryption_types = ["SSE-C"]
+    bucket_key_enabled       = false
+
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
